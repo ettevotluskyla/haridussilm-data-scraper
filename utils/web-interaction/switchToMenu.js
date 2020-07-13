1c63d4bb188b77054b3d467e7df7c2c0cb8800e7
Silver Sten Kruus <silverkruus@gmail.com>
2020-07-13 03:05:08 +0300

    Fix switchToMenu xpath

--- utils/web-interaction/switchToMenu.js
+++ utils/web-interaction/switchToMenu.js
@@ -3,7 +3,7 @@ const wrappedWaitForNetworkIdle = require('./wrappedWaitForNetworkIdle')
 
 const switchToMenu = async (page, menu) => {
   const xpath = `//*[@id="9"]/div[2]/div[1]/div[text() = '${menu}']`
-  const listTitleXPath = `//*[@id="42"]/div[1]/div[2]/div/div[text() = '${menu}']`
+  const listTitleXPath = `//*[@class='QvCaption']/div/div/div[text() = '${menu}']/../../../..`
 
   await wrappedWaitForNetworkIdle(page, [
     clickXPath(page, xpath, { waitXPath: listTitleXPath })
