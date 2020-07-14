8e240b3ac7fa0f271ebe987fbffcd59b111b1930
Silver Sten Kruus <silverkruus@gmail.com>
2020-07-14 11:27:04 +0300

    Fix waiting for simulated clicking

--- utils/web-interaction/simClickXPath.js
+++ utils/web-interaction/simClickXPath.js
@@ -1,6 +1,7 @@
 // Clicks in the middle of the provided element using the mouse
 const simClickXPath = async (page, xpath) => {
   console.log(`Simulating click on element with xpath: ${xpath}`)
+  await page.waitForXPath(xpath)
   const element = await page.$x(xpath)
 
   const bounds = await page.evaluate(el => {
